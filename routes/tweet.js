7da13d2c453c862b32f3aaeec9cb3a23151387c4
Hendrik H <hharlichs@googlemail.com>
2020-12-06 15:12:08 +0100

    refactor: move getOffers function to separate file for easier reuse

--- routes/tweet.js
+++ routes/tweet.js
@@ -1,13 +1,10 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
 require("dotenv").config();
 const Twitter = require("twitter-lite");
+const tools = require("../tools");
 
 /////////////
 // D A T A //
 /////////////
-
-const url = "https://www.durstexpress.de/hamburg/";
 
 const client = new Twitter({
 	subdomain: "api", // "api" is the default (change for other subdomains)
@@ -22,44 +19,6 @@ const client = new Twitter({
 // H E L P //
 /////////////
 
-const getOffers = async () => {
-	try {
-		const response = await axios.get(url);
-
-		const $ = await cheerio.load(response.data); // CREATE CHEERIO OBJECT FROM URL
-
-		const offerItemsMap = $("div.products-list-sale div.product-item-details") // PARSE NAMES AND PRICES OF ITEMS
-			.map(async (i, elem) => {
-				let itemName = $(elem).children().first().children().text().replace("\n", "").replace(" l ", " l");
-				let offerPrice = parseFloat($(elem).children().first().next().children().first().find($(".price-wrapper")).attr("data-price-amount"));
-				let offerPriceDisplay = $(elem).children().first().next().children().first().children().children().next().text();
-				let regularPrice = parseFloat($(elem).children().first().next().children().last().find($(".price-wrapper")).attr("data-price-amount"));
-				let regularPriceDisplay = $(elem).children().first().next().children().last().children().children().next().text();
-				let discount = parseInt($(elem).parent().before().find($(".prozent-ersparnis")).text().replace(" %", ""));
-				let discountDisplay = $(elem).parent().before().find($(".prozent-ersparnis")).text();
-
-				return {
-					itemName,
-					offer: {
-						offerPrice,
-						offerPriceDisplay,
-					},
-					regular: {
-						regularPrice,
-						regularPriceDisplay,
-					},
-					discount,
-					discountDisplay,
-				};
-			})
-			.get();
-
-		return Promise.all(offerItemsMap);
-	} catch (err) {
-		throw err;
-	}
-};
-
 async function sendTweet(content) {
 	await client.post("statuses/update", { status: content }).catch((err) => console.log(err));
 }
@@ -69,7 +28,8 @@ async function sendTweet(content) {
 /////////////
 
 module.exports = async (req, res) => {
-	getOffers()
+	tools
+		.getOffers("hamburg")
 		.then((results) => {
 			const today = new Date();
 			const date = today.getDate() + "." + (today.getMonth() + 1) + "." + today.getFullYear();
