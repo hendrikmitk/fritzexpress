066f89633f23d7e37bac9f334d3151cd871b9ba3
hendrikmitk <hharlichs@googlemail.com>
2021-01-13 21:12:32 +0100

    Add hamburg2 to areas

--- src/tools.js
+++ src/tools.js
@@ -1,44 +1,45 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 /////////////
 // D A T A //
 /////////////
 
-const url = "https://www.durstexpress.de/";
+const url = 'https://www.durstexpress.de/';
 
 const areas = [
-	{ name: "hamburg" },
-	{ name: "hannover" },
-	{ name: "bochum" },
-	{ name: "wiesbaden" },
-	{ name: "berlin2" },
-	{ name: "leipzig" },
-	{ name: "dresden" },
-	{ name: "nuernberg" },
-	{ name: "augsburg" },
-	{ name: "muenchen" },
+	{ name: 'hamburg' },
+	{ name: 'hamburg2' },
+	{ name: 'hannover' },
+	{ name: 'bochum' },
+	{ name: 'wiesbaden' },
+	{ name: 'berlin2' },
+	{ name: 'leipzig' },
+	{ name: 'dresden' },
+	{ name: 'nuernberg' },
+	{ name: 'augsburg' },
+	{ name: 'muenchen' },
 ];
 
 /////////////
 // H E L P //
 /////////////
 
-const getOffers = async (area) => {
+const getOffers = async area => {
 	try {
 		const response = await axios.get(url + area);
 
 		const $ = await cheerio.load(response.data); // CREATE CHEERIO OBJECT FROM URL
 
-		const offerItemsMap = $("div.products-list-sale div.product-item-details") // PARSE NAMES AND PRICES OF ITEMS
+		const offerItemsMap = $('div.products-list-sale div.product-item-details') // PARSE NAMES AND PRICES OF ITEMS
 			.map(async (i, elem) => {
-				let itemName = $(elem).children().first().children().text().replace("\n", "").replace(" l ", " l");
-				let offerPrice = parseFloat($(elem).children().first().next().children().first().find($(".price-wrapper")).attr("data-price-amount"));
+				let itemName = $(elem).children().first().children().text().replace('\n', '').replace(' l ', ' l');
+				let offerPrice = parseFloat($(elem).children().first().next().children().first().find($('.price-wrapper')).attr('data-price-amount'));
 				let offerPriceDisplay = $(elem).children().first().next().children().first().children().children().next().text();
-				let regularPrice = parseFloat($(elem).children().first().next().children().last().find($(".price-wrapper")).attr("data-price-amount"));
+				let regularPrice = parseFloat($(elem).children().first().next().children().last().find($('.price-wrapper')).attr('data-price-amount'));
 				let regularPriceDisplay = $(elem).children().first().next().children().last().children().children().next().text();
-				let discount = parseInt($(elem).parent().before().find($(".prozent-ersparnis")).text().replace(" %", ""));
-				let discountDisplay = $(elem).parent().before().find($(".prozent-ersparnis")).text();
+				let discount = parseInt($(elem).parent().before().find($('.prozent-ersparnis')).text().replace(' %', ''));
+				let discountDisplay = $(elem).parent().before().find($('.prozent-ersparnis')).text();
 
 				return {
 					itemName,
@@ -62,28 +63,28 @@ const getOffers = async (area) => {
 	}
 };
 
-const validateArea = async (area) => {
-	if (areas.some((i) => i.name.includes(area))) {
+const validateArea = async area => {
+	if (areas.some(i => i.name.includes(area))) {
 		return area;
 	}
 };
 
-const beautifyAreaName = (str) => {
-	const beautified = str.replace("ue", "ü").replace("ae", "ä").replace("oe", "ö").replace(/[0-9]/g, ""); // REPLACE UMLAUTS AND REMOVE NUMBERS
+const beautifyAreaName = str => {
+	const beautified = str.replace('ue', 'ü').replace('ae', 'ä').replace('oe', 'ö').replace(/[0-9]/g, ''); // REPLACE UMLAUTS AND REMOVE NUMBERS
 	const capitalized = beautified[0].toUpperCase() + beautified.slice(1);
 	return capitalized;
 };
 
-const createErrorObject = (param) => {
+const createErrorObject = param => {
 	const areasArray = [];
-	areas.forEach((area) => {
+	areas.forEach(area => {
 		areasArray.push(area.name);
 	});
 	const errorObject = [
 		{
 			code: 409,
-			text: "Conflict",
-			description: "Invalid route parameter",
+			text: 'Conflict',
+			description: 'Invalid route parameter',
 			data: {
 				requested: param,
 				available: areasArray,
@@ -93,13 +94,13 @@ const createErrorObject = (param) => {
 	return errorObject;
 };
 
-const handleNetworkError = (error) => {
+const handleNetworkError = error => {
 	if (error.response) {
 		console.error(error.response.status);
 	} else if (error.request) {
 		console.error(error.request);
 	} else {
-		console.error("Error", error.message);
+		console.error('Error', error.message);
 	}
 };
 
